2738d92b091239a0730e716475ab070d3e45be11
Tatsuya-kanaria <kanaria.git@gmail.com>
2022-11-27 20:49:14 +0900

    add cart history index

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,8 @@ use App\Models\User;
 use App\Models\Product;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use App\Models\ShoppingCart;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class UserController extends Controller
 {
@@ -90,5 +92,21 @@ class UserController extends Controller
 
         Auth::logout();
         return redirect('/');
+    }
+
+    public function cart_history_index(Request $request)
+    {
+        $page = $request->page !== null ? $request->page : 1;
+        $user_id = Auth::user()->id;
+        $billings = ShoppingCart::getCurrentUserOrders($user_id);
+        $total = count($billings);
+        $billings = new LengthAwarePaginator(array_slice($billings, ($page - 1) * 15, 15), $total, 15, $page, array('path' => $request->url()));
+
+        return view('users.cart_history_index', compact('billings', 'total'));
+    }
+
+    public function cart_history_show(Request $request)
+    {
+        $num = $request->num;
     }
 }
